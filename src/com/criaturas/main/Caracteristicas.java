14ea90593fed2ddd2228737f936898464247ccd5
ianariel200 <ianariel200@gmail.com>
2014-05-10 21:18:48 -0300

    Caracteristicas
    Subo los cambios de caracterisiticas

---
+++ src/com/criaturas/main/Caracteristicas.java
@@ -0,0 +1,7 @@
+package com.criaturas.main;
+
+public class Caracteristicas {
+	public double vida;
+	public double mana;
+	public String arma;
+	}
---
+++ src/com/criaturas/main/Personajes.java
@@ -0,0 +1,9 @@
+package com.criaturas.main;
+
+public class Personajes extends Caracteristicas {
+	public String Hechicero;
+	public String Elfos;
+	public String Orcos;
+	public String Humanos;
+	
+}
